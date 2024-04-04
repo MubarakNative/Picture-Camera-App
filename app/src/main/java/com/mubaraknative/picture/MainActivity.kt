f94be490e0f7027512056d4370cb13bbd2cce6f9
Mubarak <enpisb251@gmail.com>
2024-04-04 14:06:37 +0530

    requested runtime camera permission

--- app/src/main/java/com/mubaraknative/picture/MainActivity.kt
+++ app/src/main/java/com/mubaraknative/picture/MainActivity.kt
@@ -1,22 +1,66 @@
 package com.mubaraknative.picture
 
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.camera.view.LifecycleCameraController
+import androidx.core.content.ContextCompat
+import com.mubaraknative.picture.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var cameraController: LifecycleCameraController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        if (allPermissionsGranted()) {
+            executeCamera()
+        } else { // request permission
+            activityResultLauncher.launch(permissions)
         }
 
     }
+
+    private val activityResultLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { permissions ->
+
+        var isPermissionGranted: Boolean = true
+        permissions.entries.forEach {
+            if (it.key in this.permissions && !it.value) {
+                isPermissionGranted = false
+            }
+        }
+        if (!isPermissionGranted) {
+            // Todo: show educational UI why this permission needed
+        } else {
+            // permission granted
+            executeCamera()
+        }
+    }
+
+
+    private fun executeCamera() {
+
+    }
+
+    private fun allPermissionsGranted() = permissions.all {
+        ContextCompat.checkSelfPermission(this, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+        private val permissions =
+            mutableListOf(
+                android.Manifest.permission.CAMERA
+            ).apply {
+                if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+                    add(android.Manifest.permission.WRITE_EXTERNAL_STORAGE) // this permission is only required on android 9 and below
+                }
+            }.toTypedArray()
+
 }
