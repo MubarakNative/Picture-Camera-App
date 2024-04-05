2e384bd5b95e78e372240976367b8aaec72587eb
Mubarak <enpisb251@gmail.com>
2024-04-05 19:08:24 +0530

    Handled Permission denial and implemented camera preview

--- app/src/main/java/com/mubaraknative/picture/MainActivity.kt
+++ app/src/main/java/com/mubaraknative/picture/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.mubaraknative.picture
 
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
-import android.widget.Toast
+import android.provider.Settings
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.CameraSelector
 import androidx.camera.view.LifecycleCameraController
 import androidx.core.content.ContextCompat
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.mubaraknative.picture.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -38,7 +42,7 @@ class MainActivity : AppCompatActivity() {
             }
         }
         if (!isPermissionGranted) {
-            // Todo: show educational UI why this permission needed
+            showEducationalDialog()
         } else {
             // permission granted
             executeCamera()
@@ -47,7 +51,11 @@ class MainActivity : AppCompatActivity() {
 
 
     private fun executeCamera() {
-
+        val cameraPreview = binding.previewView
+        cameraController = LifecycleCameraController(this)
+        cameraController.bindToLifecycle(this)
+        cameraController.cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+        cameraPreview.controller = cameraController
     }
 
     private fun allPermissionsGranted() = permissions.all {
@@ -62,5 +70,25 @@ class MainActivity : AppCompatActivity() {
                     add(android.Manifest.permission.WRITE_EXTERNAL_STORAGE) // this permission is only required on android 9 and below
                 }
             }.toTypedArray()
+
+
+    private fun showEducationalDialog(){
+        val dialog = MaterialAlertDialogBuilder(this)
+            .setTitle(R.string.permission_denied_title)
+            .setMessage(R.string.permission_denied_msg)
+            .setNegativeButton(R.string.close) { dialog, _ ->
+                dialog.dismiss()
+                finish()
+            }
+            .setPositiveButton(R.string.settings){dialog,_->
+                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                val uri = Uri.fromParts("package", packageName, null)
+                intent.setData(uri)
+                startActivity(intent)
+                dialog.dismiss()
+            }
+            .setCancelable(false)
+        dialog.show()
+    }
 
 }
